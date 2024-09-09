a8928f13726fcc7a2d8c5708c0aeaedb5fa40e46
ibraloyza <ibraahim3523@gmail.com>
2024-09-10 02:23:47 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './components/Navbar/Navbar'
+import Hero from './components/Hero/Hero'
+
+function App() {
+  return (
+    <main className=" overflow-hidden bg-white text-dark">
+      <Navbar />
+ 
+    </main>
+  )
+}
+
+export default App
